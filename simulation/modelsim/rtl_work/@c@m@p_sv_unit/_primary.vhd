56d2569fe1140890601aa5cd8084d67188b6667e
Jeff Ma <yuanm2@dcl-l440-30.ews.illinois.edu>
2018-10-17 20:43:38 -0500

    remove unnecessary files

--- simulation/modelsim/rtl_work/@c@m@p_sv_unit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity CMP_sv_unit is
-end CMP_sv_unit;
--- simulation/modelsim/rtl_work/@m@d@r_sv_unit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity MDR_sv_unit is
-end MDR_sv_unit;
