dc996575d1a5969ebf7a5f226200645d9a04c22c
Ken Xu <hxu52@siebl-0218-29.ews.illinois.edu>
2018-10-19 21:12:05 -0500

    modified wb_pipe

--- WB_pipe.sv
+++ WB_pipe.sv
@@ -10,7 +10,7 @@ module WB_pipe
 	output logic [31:0] WB_pc,
 	/*other signals*/
 	input clk,
-	input load,
+	//input load,
 	input reset
 );
 
@@ -20,12 +20,14 @@ always_ff @(posedge clk) begin
 	WB_alu_out<=0;
 	WB_rdata<=0;
 	WB_pc<=0;
+	WB_cmp_out<=0;
 	end
 
 	else begin
 	WB_alu_out<=MEM_alu_out;
 	WB_rdata<=MEM_rdata;
 	WB_pc<=MEM_pc;
+	WB_cmp_out<=MEM_cmp_out;
 	end
 end
 
