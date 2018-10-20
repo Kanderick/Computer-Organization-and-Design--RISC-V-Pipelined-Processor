60cc86b69963da7144e7bfebd46355c22cda02dd
Jeff Ma <yuanm2@siebl-0218-28.ews.illinois.edu>
2018-10-19 22:22:01 -0500

    add pcmux_sel to EX_pipe.sv

--- EX_pipe.sv
+++ EX_pipe.sv
@@ -3,9 +3,11 @@ module EX_pipe
 	input [31:0] ID_pc,
 	input [31:0] ID_rs1_out,
 	input [31:0] ID_rs2_out,
+	input pcmux_sel,
 	output logic [31:0] EX_pc,
 	output logic [31:0] EX_rs1_out,
 	output logic [31:0] EX_rs2_out,
+	output logic EX_pcmux_sel,
 	/*other signals*/
 	input clk,
 	input reset
@@ -16,11 +18,13 @@ always_ff @ (posedge clk) begin
 	EX_pc<=0;
 	EX_rs1_out<=0;
 	EX_rs2_out<=0;
+	EX_pcmux_sel<=0;
 	end
 	else begin
 	EX_pc<=ID_pc;
 	EX_rs1_out<=ID_rs1_out;
 	EX_rs2_out<=ID_rs2_out;
+	EX_pcmux_sel<=pcmux_sel;
 	end
 end
 endmodule : EX_pipe
