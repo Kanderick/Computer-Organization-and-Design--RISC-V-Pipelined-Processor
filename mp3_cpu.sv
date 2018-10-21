194d9dc9cd2d522f82b2cd201914414f47eb9543
Haiyang Zhang <haiyang3@siebl-0220-01.ews.illinois.edu>
2018-10-20 23:15:34 -0500

    start integration

--- mp3_cpu.sv
+++ mp3_cpu.sv
@@ -4,162 +4,214 @@ module mp3_cpu
 (
     input clk,
 
-    /* Memory signals */
-    input mem_resp,
-    input [31:0] mem_rdata,
-    output mem_read,
-    output mem_write,
-    output [3:0] mem_byte_enable,
-    output [31:0] mem_address,
-    output [31:0] mem_wdata
+    /* instruction cache */
+    output logic read_a,
+    output logic write_a,
+    output logic [3:0] wmask_a,
+    output logic [31:0] address_a,
+    output logic [31:0] wdata_a,
+    input resp_a,
+    input [31:0] rdata_a,
+
+    /* data cache */
+    output logic read_b,
+    output logic write_b,
+    output logic [3:0] wmask_b,
+    output logic [31:0] address_b,
+    output logic [31:0] wdata_b,
+    input resp_b,
+    input [31:0] rdata_b
 );
 
-rv32i_opcode opcode;
-logic [2:0] funct3;
-logic [6:0] funct7;
-logic bit30;
-logic br_en;
-logic load_pc;
-logic load_ir;
-logic load_regfile;
-logic load_MAR;
-logic load_MDR;
-logic load_mem_data_out;
-logic pcmux_sel;
-logic [2:0] regfilemux_sel;
-logic marmux_sel;
-alu_ops aluop;
-logic alumux1_sel;
-logic [2:0] alumux2_sel;
-branch_funct3_t cmpop;
-logic cmpmux_sel;
-logic load_data_out;
-logic jalr;
-logic instr_data;
+/*IF_stage signal*/
+logic [31:0] IF_addr;
+logic {31:0} instr;
 
-/* Instantiate MP 0 top level blocks here */
-/*
-module control
+/*ID_stage signal*/
+
+/*EX_stage signal*/
+
+/*MEM_stage signal*/
+
+/*WB_stage signal*/
+
+/*control word*/
+rv32i_control_word ID_ctrl;
+rv32i_control_word EX_ctrl;
+rv32i_control_word MEM_ctrl;
+rv32i_control_word WB_ctrl;
+
+IF_stage IF_stage
 (
-    /* Input and output port declarations *
-	 input clk,
-	 /* datapath controls *
-	 /* ir *
-	 input rv32i_opcode opcode,
-	 input [2:0] funct3,
-    input [6:0] funct7,
-	 /* CMP *
-	 input br_en,
-	 /* load signal *
-	 output logic load_pc,
-	 output logic load_ir,
-	 output logic load_regfile,
-	 output logic load_MAR,
-	 output logic load_MDR,
-	 output logic load_mem_data_out,
-	 /* select signal *
-	 output logic pcmux_sel,
-	 output logic marmux_sel,
-	 output logic alumux1_sel,
-	 output logic cmpmux_sel,
-	 output logic [1:0] alumux2_sel,
-	 output logic [1:0] regfilemux_sel
-	 /* alu signal *
-	 output logic alu_ops aluop,
-	 /* CMP signal *
-	 output logic branch_funct3_t cmpop,
-	 /* memory signals *
-	 input mem_resp,
-	 output logic mem_read,
-	 output logic mem_write,
-	 output rv32i_mem_wmask mem_byte_enable
+		.clk,
+		.EX_jmp_pc,
+		.IF_addr
 );
-*/
-cpu_control control
+
+control_memory control_memory
 (
+    .instr,
+    .ctrl
+);
+
+ module control_word_reg
+ (
     .clk,
-	 .opcode,
-	 .funct3,
-	 .funct7,
-	 .br_en,
-	 .load_pc,
-	 .load_ir,
-	 .load_regfile,
-	 .load_MAR,
-	 .load_MDR,
-	 .load_mem_data_out,
-	 .pcmux_sel,
-	 .marmux_sel,
-	 .alumux1_sel,
-	 .cmpmux_sel,
-	 .alumux2_sel,
-	 .regfilemux_sel,
-	 .aluop,
-	 .cmpop,
-	 .mem_resp,
-	 .mem_read,
-	 .mem_write,
-	 .mem_byte_enable,
-	 .instr_data
+    .reset,
+    .rv32i_control_word control_signal_in,
+    output rv32i_control_word control_signal_out, 
+    input load_control_word 
+ );
+ 
+module ID_pipe
+(
+	input [31:0] IF_pc,
+	output logic [31:0] ID_pc,
+	input clk,
+	input reset
 );
-/*
-module datapath
+
+module ID_stage
+(
+		input clk,
+		input [4:0] ID_rs1,
+		input [4:0] ID_rs2,
+		input [31:0] WB_in,
+		input [4:0] ID_rd,
+		input ID_load_regfile,
+		input [31:0] ID_pc,
+		input [31:0] ID_b_imm,
+		input [31:0] ID_j_imm,
+		input [31:0] ID_i_imm,
+		input [1:0] jb_sel,
+		
+		output logic [31:0] ID_rs1_out,
+		output logic [31:0] ID_rs2_out,
+		output logic [31:0] ID_jmp_pc,
+		output logic [31:0] ID_pc_out
+);
+
+ module control_word_reg
+ (
+    input clk,
+    input reset,
+    input rv32i_control_word control_signal_in,
+    output rv32i_control_word control_signal_out, 
+    input load_control_word 
+ );
+
+module EX_pipe
+(
+	input [31:0] ID_pc,
+	input [31:0] ID_rs1_out,
+	input [31:0] ID_rs2_out,
+	input [31:0] ID_jmp_pc,
+	input ID_pc_mux_sel,
+	
+	output logic [31:0] EX_pc,
+	output logic [31:0] EX_rs1_out,
+	output logic [31:0] EX_rs2_out,
+	output logic [31:0] EX_jmp_pc,
+	output logic EX_pc_mux_sel,
+	/*other signals*/
+	input clk,
+	input reset
+);
+
+module EX_stage
 (
     input clk,
-    /* select signals *
-    input pcmux_sel,
-	 input marmux_sel,
-	 input alumux1_sel,
-	 input cmpmux_sel
-	 input [1:0] alumux2_sel,
-	 input [1:0] regfilemux_sel,
-	 /* control signals *
-    input load_pc,
-	 input load_ir,
-	 input load_MDR, 
-	 input load_MAR,
-	 input load_regfile,
-	 input load_mem_data_out,
-	 /* ALU info signal *
-	 input alu_ops aluop,
-	 input branch_funct3_t cmpop,
-	 /* memory input *
-	 input rv32i_word mem_rdata,
-	 
-	 /* output signals *
-	 output logic [2:0] funct3,
-    output logic [6:0] funct7,
-    output rv32i_opcode opcode,
-	 output rv32i_word mem_address
-	 output rv32i_word mem_wdata,
-	 output logic br_en;
+    /* control signals */
+    input logic EX_alumux1_sel,
+    input logic [2:0] EX_alumux2_sel,
+    input logic [2:0] EX_regfilemux_sel,
+    input logic EX_cmpmux_sel,
+    input alu_ops EX_aluop,
+    input branch_funct3_t EX_cmpop,
+    input load_funct3_t EX_loadop,
+    /* input data*/
+    input rv32i_word EX_pc,
+    input rv32i_word EX_rs1_out, EX_rs2_out,
+    input rv32i_word EX_i_imm, EX_u_imm, EX_b_imm, EX_s_imm, EX_j_imm,
+    input rv32i_word EX_rs1_forwarded_WB, EX_rs2_forwarded_WB,
+    input rv32i_word EX_rs1_forwarded_MEM, EX_rs2_forwarded_MEM,    
+    /*output data*/
+    output rv32i_word EX_alu_out,
+    output rv32i_word EX_cmp_out,
+    /*to do*/
+    input logic EX_forwarding_sel1,
+    input logic EX_forwarding_sel2
 );
-*/
-cpu_datapath datapath
+
+ module control_word_reg
+ (
+    input clk,
+    input reset,
+    input rv32i_control_word control_signal_in,
+    output rv32i_control_word control_signal_out, 
+    input load_control_word 
+ );
+ 
+module MEM_pipe(
+	input clk,
+	input reset,
+	// input load,
+	
+	input [31:0] EX_pc,
+	input [31:0] EX_alu_out,
+	input [31:0] EX_rs2_out,
+	input EX_cmp_out,
+	
+	output logic [31:0] MEM_pc,
+	output logic [31:0] MEM_alu_out,
+	output logic [31:0] MEM_rs2_out,
+	output MEM_cmp_out
+);
+
+module MEM_stage
 (
-    .clk,
-    .pcmux_sel,
-	 .marmux_sel,
-	 .alumux1_sel,
-	 .cmpmux_sel,
-	 .alumux2_sel,
-	 .regfilemux_sel,
-    .load_pc,
-	 .load_ir,
-	 .load_MDR, 
-	 .load_MAR,
-	 .load_regfile,
-	 .load_mem_data_out,
-	 .aluop,
-	 .cmpop,
-	 .mem_rdata,
-	 .funct3,
-    .funct7,
-    .opcode,
-	 .mem_address,
-	 .mem_wdata,
-	 .br_en,
-	 .instr_data
+	input [31:0] MEM_rs2_out,
+	input [31:0] MEM_alu_out,
+	output [31:0] MEM_addr,
+	output [31:0] MEM_data
+
+);	
+
+ module control_word_reg
+ (
+    input clk,
+    input reset,
+    input rv32i_control_word control_signal_in,
+    output rv32i_control_word control_signal_out, 
+    input load_control_word 
+ );
+ 
+module WB_pipe
+(
+	input MEM_cmp_out,
+	input [31:0] MEM_alu_out,
+	input [31:0] MEM_rdata,	/*read from data cache*/
+	input [31:0] MEM_pc,
+	output logic WB_cmp_out,
+	output logic [31:0] WB_alu_out,
+	output logic [31:0] WB_rdata,
+	output logic [31:0] WB_pc,
+	/*other signals*/
+	input clk,
+	//input load,
+	input reset
 );
+
+module WB_stage(
+	input [31:0] WB_pc,
+	input [2:0] WB_funct3,
+	input [31:0] WB_rdata,
+	input WB_cmp_out,
+	input [31:0] WB_alu_out,
+	input [31:0] WB_u_imm,
+	input [3:0] WB_regfilemux_sel,
+	output logic [31:0] WB_in
+);
+
 
 endmodule : mp3_cpu
